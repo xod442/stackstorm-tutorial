79a6a98beaa821e78b3eb01dc68841d83ef078fa
rick kauffman <rick@rickkkauffman.com>
2019-09-05 21:13:10 -0400

    write_html-working

--- etc/answers/actions/write_html.py
+++ etc/answers/actions/write_html.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 #
 # Description:
-#   Gets a link for a picture. And writes a html link to an index file..
+#   a stackstorm action that takes a link string and formats as a hypelink and
+#   writes to the /etc/index.html.
 # Author:
 #   Rick Kauffman wookieware.com
 import os
 import argparse
 import json
 import requests
+from st2common.runners.base_action import Action
 
 
 class WriteHtml(Action):
 
     def run(self,link):
-        params = {'api_key': api_key,
-                  'hd': hd}
         if link is not None:
             # write to a index file
-            file1=open("/etc/index.html","w")
-            line = "<a href="+link+">"+link+"</a>"
+            file1=open("/opt/stackstorm/packs/tutorial/etc/index.html","a")
+            line = "<p><a href="+link+">"+link+"</a></p>\n"
 
             file1.write(line)
             file1.close()
